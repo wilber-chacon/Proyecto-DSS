7874249b98bdebf56535c474920debceb0d1d1ea
wilber-chacon <wilber212016erroa@gmail.com>
2023-03-07 13:51:14 -0600

    Estilo y estructura

--- gerente_sucursal/superior.php
+++ gerente_sucursal/superior.php
@@ -9,7 +9,7 @@
     <link
         href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
         rel="stylesheet" />
-    <link href="css/all.min.css" rel="stylesheet" type="text/css" />
+    <link href="../css/all.min.css" rel="stylesheet" type="text/css" />
     <link rel="stylesheet" href="../css/estilos.css">
     <link rel="stylesheet" href="../css/bootstrap.css">
     <link rel="stylesheet" href="../css/alertify.core.css">
