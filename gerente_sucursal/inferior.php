2f6bf3a602b4bad9bbfa2b3b61b7f92a73c9fd0b
wilber-chacon <wilber212016erroa@gmail.com>
2023-03-07 13:25:01 -0600

    Estilo y estructura

---
+++ gerente_sucursal/inferior.php
@@ -0,0 +1,19 @@
+
+
+<footer class="sticky-footer bg-dark">
+        <div class="my-auto">
+            <div class="copyright pt-4 pb-4 text-center my-auto text-white">
+                <span>Copyright &copy; 2023 - Banco de Agricultura Salvadoreño SA de CV</span>
+            </div>
+        </div>
+    </footer>
+
+    <script src="../js/jquery-1.12.0.min.js"></script>
+    <script src="../js/jquery-3.2.1.slim.min.js"></script>
+    <script src="../js/jquery.min.js"></script>
+    <script src="../js/bootstrap.bundle.js"></script>
+    <script src="../js/bootstrap.js"></script>
+    <script src="../js/alertify.js"></script>
+</body>
+
+</html>
---
+++ gerente_sucursal/superior.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Banco de agricultura</title>
+    <link
+        href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
+        rel="stylesheet" />
+    <link href="css/all.min.css" rel="stylesheet" type="text/css" />
+    <link rel="stylesheet" href="../css/estilos.css">
+    <link rel="stylesheet" href="../css/bootstrap.css">
+    <link rel="stylesheet" href="../css/alertify.core.css">
+    <link rel="stylesheet" href="../css/alertify.default.css">
+    <link rel="stylesheet" href="../css/style.css">
+</head>
+
+<body>
+
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <a class="navbar-brand" href="#">
+            <img src="../img/logo.png" width="30" height="30" class="d-inline-block align-top" alt="">
+            Banco de Agricultura
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown"
+            aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavDropdown">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Inicio</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
