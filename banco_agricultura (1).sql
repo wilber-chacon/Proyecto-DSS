cd82b49cf9617090cfc2c5af83bd856b2d092580
Edwards28 <davis12merlos@gmail.com>
2023-03-13 23:51:12 -0600

    ¨BDDActualizada¨

--- banco_agricultura (1).sql
+++ banco_agricultura (1).sql
@@ -36,7 +36,13 @@ CREATE TABLE IF NOT EXISTS `cliente` (
   CONSTRAINT `sesionfk_3` FOREIGN KEY (`codigo_sesion`) REFERENCES `sesiones` (`codigo_sesion`)
 ) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=latin1;
 
--- La exportación de datos fue deseleccionada.
+-- Volcando datos para la tabla banco_agricultura.cliente: ~4 rows (aproximadamente)
+DELETE FROM `cliente`;
+INSERT INTO `cliente` (`codigo_cliente`, `nombre_cliente`, `DUI_cliente`, `correo_cliente`, `telefono_cliente`, `domicilio_cliente`, `fechaNacimiento_cliente`, `sueldoCliente`, `codigo_sesion`) VALUES
+	(11, 'Davis Merlos', '06523406-5', 'davis15merlos@gmail.com', '7956-9665', 'San Salvador', '2002-07-25', 600.00, 10),
+	(12, 'Roberto Vásquez', '06231336-2', 'desarrollo@acaces.com.sv', '7953-5632', 'San Marcos', '1992-09-09', 600.00, 11),
+	(13, 'Elena Landaverde', '06532565-6', 'marialandaverde1993@gmail.com', '7956-6262', 'Apopa', '1993-12-26', 966.00, 12),
+	(14, 'Mario Hernandez', '27482542-9', 'mario.hernandez@gmail.com', '7245-3422', 'Soyapango', '1996-03-15', 545.75, 13);
 
 -- Volcando estructura para tabla banco_agricultura.cuentabancaria
 CREATE TABLE IF NOT EXISTS `cuentabancaria` (
@@ -51,7 +57,8 @@ CREATE TABLE IF NOT EXISTS `cuentabancaria` (
   CONSTRAINT `clientefk_2` FOREIGN KEY (`codigo_cliente`) REFERENCES `cliente` (`codigo_cliente`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- La exportación de datos fue deseleccionada.
+-- Volcando datos para la tabla banco_agricultura.cuentabancaria: ~0 rows (aproximadamente)
+DELETE FROM `cuentabancaria`;
 
 -- Volcando estructura para tabla banco_agricultura.dependiente
 CREATE TABLE IF NOT EXISTS `dependiente` (
@@ -68,7 +75,8 @@ CREATE TABLE IF NOT EXISTS `dependiente` (
   CONSTRAINT `sesionfk_2` FOREIGN KEY (`codigo_sesion`) REFERENCES `sesiones` (`codigo_sesion`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- La exportación de datos fue deseleccionada.
+-- Volcando datos para la tabla banco_agricultura.dependiente: ~0 rows (aproximadamente)
+DELETE FROM `dependiente`;
 
 -- Volcando estructura para tabla banco_agricultura.empleados
 CREATE TABLE IF NOT EXISTS `empleados` (
@@ -93,7 +101,12 @@ CREATE TABLE IF NOT EXISTS `empleados` (
   CONSTRAINT `sucursalfk_1` FOREIGN KEY (`codigo_sucursal`) REFERENCES `sucursal` (`codigo_sucursal`)
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 
--- La exportación de datos fue deseleccionada.
+-- Volcando datos para la tabla banco_agricultura.empleados: ~3 rows (aproximadamente)
+DELETE FROM `empleados`;
+INSERT INTO `empleados` (`codigo_empleado`, `nombre_empleado`, `DUI_empleado`, `correo_empleado`, `telefono_empleado`, `Estado_empleado`, `domicilio_empleado`, `acciones`, `fechaNacimiento_empleado`, `codigo_rol`, `codigo_sesion`, `codigo_sucursal`) VALUES
+	(1, 'Wilber Francisco Chacón Erroa', '12345678-9', 'wilber.franciscochacon@gmail.com', '6543-4352', 'Activo', 'Soyapango', 'All', '2002-03-21', 5, 14, 3),
+	(2, 'Juan Guillermo Juárez', '05451244-5', 'juan@gmail.com', '9562-5653', 'Activo', 'Soyapango', 'All', '2023-03-13', 7, 15, 4),
+	(3, 'Pedro', '06532665-5', 'pedro@gmail.com', '7895-5653', 'Activo', 'San Salvador', 'All', '2000-09-03', 1, 16, 1);
 
 -- Volcando estructura para tabla banco_agricultura.movimientos
 CREATE TABLE IF NOT EXISTS `movimientos` (
@@ -108,7 +121,8 @@ CREATE TABLE IF NOT EXISTS `movimientos` (
   CONSTRAINT `cuentafk_2` FOREIGN KEY (`numCuenta`) REFERENCES `cuentabancaria` (`numCuenta`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- La exportación de datos fue deseleccionada.
+-- Volcando datos para la tabla banco_agricultura.movimientos: ~0 rows (aproximadamente)
+DELETE FROM `movimientos`;
 
 -- Volcando estructura para tabla banco_agricultura.prestamos
 CREATE TABLE IF NOT EXISTS `prestamos` (
@@ -125,7 +139,10 @@ CREATE TABLE IF NOT EXISTS `prestamos` (
   CONSTRAINT `clientefk_1` FOREIGN KEY (`codigo_cliente`) REFERENCES `cliente` (`codigo_cliente`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 
--- La exportación de datos fue deseleccionada.
+-- Volcando datos para la tabla banco_agricultura.prestamos: ~1 rows (aproximadamente)
+DELETE FROM `prestamos`;
+INSERT INTO `prestamos` (`numPrestamo`, `estado_prestamo`, `fechaApertura`, `monto_prestamo`, `porcentajeInteres`, `cuotaMensual`, `cantYearAPagar`, `codigo_cliente`) VALUES
+	(1, 'Aprobado', '2023-03-10', 2000.00, 3, 58.16, 3, 11);
 
 -- Volcando estructura para tabla banco_agricultura.roles
 CREATE TABLE IF NOT EXISTS `roles` (
@@ -134,7 +151,17 @@ CREATE TABLE IF NOT EXISTS `roles` (
   PRIMARY KEY (`codigo_rol`)
 ) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
 
--- La exportación de datos fue deseleccionada.
+-- Volcando datos para la tabla banco_agricultura.roles: ~8 rows (aproximadamente)
+DELETE FROM `roles`;
+INSERT INTO `roles` (`codigo_rol`, `nombre_rol`) VALUES
+	(1, 'Cajero'),
+	(2, 'Limpieza'),
+	(3, 'Recepcionista'),
+	(4, 'Mesa'),
+	(5, 'Gerente de sucursal'),
+	(6, 'Gerente general del banco'),
+	(7, 'Cliente'),
+	(8, 'Dependiente del banco');
 
 -- Volcando estructura para tabla banco_agricultura.sesiones
 CREATE TABLE IF NOT EXISTS `sesiones` (
@@ -146,7 +173,16 @@ CREATE TABLE IF NOT EXISTS `sesiones` (
   PRIMARY KEY (`codigo_sesion`)
 ) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=latin1;
 
--- La exportación de datos fue deseleccionada.
+-- Volcando datos para la tabla banco_agricultura.sesiones: ~7 rows (aproximadamente)
+DELETE FROM `sesiones`;
+INSERT INTO `sesiones` (`codigo_sesion`, `usuario`, `pass`, `cod_verificacion`, `date_verficacion`) VALUES
+	(10, 'dmerlos', 'e10adc3949ba59abbe56e057f20f883e', '244013', '2023-03-10 11:13:39'),
+	(11, 'rvasquez', 'e10adc3949ba59abbe56e057f20f883e', NULL, NULL),
+	(12, 'elandaverde', 'e10adc3949ba59abbe56e057f20f883e', '102396', '2023-03-10 11:42:23'),
+	(13, 'mario.hernandez@gmail.com', 'Ãä÷Ù¹¨Oîû|¿AaÛo', NULL, NULL),
+	(14, 'wilber.franciscochacon@gmail.com', 'Ãä÷Ù¹¨Oîû|¿AaÛo', NULL, NULL),
+	(15, 'juan@gmail.com', 'Ãä÷Ù¹¨Oîû|¿AaÛo', NULL, NULL),
+	(16, 'pedro@gmail.com', 'Ãä÷Ù¹¨Oîû|¿AaÛo', NULL, NULL);
 
 -- Volcando estructura para tabla banco_agricultura.sucursal
 CREATE TABLE IF NOT EXISTS `sucursal` (
@@ -156,7 +192,15 @@ CREATE TABLE IF NOT EXISTS `sucursal` (
   PRIMARY KEY (`codigo_sucursal`)
 ) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
 
--- La exportación de datos fue deseleccionada.
+-- Volcando datos para la tabla banco_agricultura.sucursal: ~6 rows (aproximadamente)
+DELETE FROM `sucursal`;
+INSERT INTO `sucursal` (`codigo_sucursal`, `nombre_sucursal`, `direccion_sucursal`) VALUES
+	(1, 'Agencia plaza futura', 'Edif. Torre Futura y Plaza Futura, Col. Escalón, SS.'),
+	(2, 'Agencia masferrer', 'Final Paseo General Escalón No. 5148, S. S.'),
+	(3, 'Agencia galerias', 'Centro Com. Galerías Escalón, 1er. Nivel Local 117 y 118-A.'),
+	(4, 'Agencia bambu', 'Boulevard El Hipódromo y Avenida Las Magnolias, San Salvador'),
+	(5, 'Agencia roosevelt san salvador', '43 Avenida Norte y Alameda Franklin Delano Roosevelt No. 2222, San Salvador'),
+	(6, 'Agencia altavista', 'Ctro. Comercial Unicentro Altavista, Local 6B a 8B, Ilopango');
 
 -- Volcando estructura para tabla banco_agricultura.transferencias
 CREATE TABLE IF NOT EXISTS `transferencias` (
@@ -171,7 +215,8 @@ CREATE TABLE IF NOT EXISTS `transferencias` (
   CONSTRAINT `cuentafk_1` FOREIGN KEY (`numCuenta`) REFERENCES `cuentabancaria` (`numCuenta`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- La exportación de datos fue deseleccionada.
+-- Volcando datos para la tabla banco_agricultura.transferencias: ~0 rows (aproximadamente)
+DELETE FROM `transferencias`;
 
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
